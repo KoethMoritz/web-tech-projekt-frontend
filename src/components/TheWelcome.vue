add74541e8c52c7a4324c76cd6c88d550e79b777
Patrick.Scheibe <Patrick.Scheibe@student.htw-berlin.de>
2024-01-14 16:47:54 +0100

    Styling

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,16 +1,18 @@
 <template>
-    <WelcomeItem>
-        <template #icon>
-            <DocumentationIcon />
-        </template>
-        <template #heading>Deine Rezepte</template>
-        <input type="text" v-model="searchKeyword" placeholder="Search by name" />
-        <ul>
-            <li v-for="result in searchResults" :key="result.id">
-                <RouterLink :to="'/recipe/' + result.id">{{ result.name }}</RouterLink>
-            </li>
-        </ul>
-    </WelcomeItem>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Deine Rezepte</template>
+    <input type="text" v-model="searchKeyword" placeholder="Search by name" />
+    <ul>
+      <li v-for="result in searchResults" :key="result.id" style="list-style: none; margin-bottom: 10px;">
+        <RouterLink :to="'/recipe/' + result.id" style="color: lightgreen; font-weight: bold; text-decoration: none; font-size: 18px;">
+          {{ result.name }}
+        </RouterLink>
+      </li>
+    </ul>
+  </WelcomeItem>
 </template>
 
 <script setup lang="ts">
@@ -22,29 +24,29 @@ const recipes = ref([]);
 const searchResults = ref([]);
 
 const loadRecipes = async () => {
-    try {
-        const response = await fetch('http://localhost:8080/recipes');
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        recipes.value = data;
-    } catch (error) {
-        console.error('Error fetching recipes:', error);
+  try {
+    const response = await fetch('http://localhost:8080/recipes');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
+    const data = await response.json();
+    recipes.value = data;
+  } catch (error) {
+    console.error('Error fetching recipes:', error);
+  }
 };
 
 const searchRecipes = () => {
-    const keyword = searchKeyword.value.toLowerCase();
-    if (keyword.trim() === '') {
-        searchResults.value = [];
-    } else {
-        searchResults.value = recipes.value.filter(recipe => recipe.name.toLowerCase().includes(keyword));
-    }
+  const keyword = searchKeyword.value.toLowerCase();
+  if (keyword.trim() === '') {
+    searchResults.value = [];
+  } else {
+    searchResults.value = recipes.value.filter(recipe => recipe.name.toLowerCase().includes(keyword));
+  }
 };
 
 onMounted(() => {
-    loadRecipes();
+  loadRecipes();
 });
 
 watch(searchKeyword, searchRecipes);
