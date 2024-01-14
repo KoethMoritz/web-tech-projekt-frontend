2420d4cc94fd32082e317160f4df2f8436f70f92
Patrick.Scheibe <Patrick.Scheibe@student.htw-berlin.de>
2024-01-14 16:47:11 +0100

    Styling

--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -1,12 +1,14 @@
 <template>
-    <div style="color: white;">
-        <h2>Deine Rezepte:</h2>
-        <ul>
-            <li v-for="recipe in recipes" :key="recipe.id">
-                <router-link :to="{ name: 'recipeDetails', params: { id: recipe.id }}">{{ recipe.name }}</router-link>
-            </li>
-        </ul>
-    </div>
+  <div style="color: white;">
+    <h2>Deine Rezepte:</h2>
+    <ul>
+      <li v-for="recipe in recipes" :key="recipe.id" style="list-style: none; margin-bottom: 10px;">
+        <router-link :to="{ name: 'recipeDetails', params: { id: recipe.id }}" style="color: lightgreen; font-weight: bold; text-decoration: none; font-size: 18px;">
+          {{ recipe.name }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
@@ -17,24 +19,24 @@ const recipes = ref([]);
 const route = useRoute();
 
 const loadRecipes = () => {
-    fetch('http://localhost:8080/recipes')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Received data:', data);
-            recipes.value = data;
-        })
-        .catch(error => {
-            console.error('Error fetching recipes:', error);
-        });
+  fetch('http://localhost:8080/recipes')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Received data:', data);
+        recipes.value = data;
+      })
+      .catch(error => {
+        console.error('Error fetching recipes:', error);
+      });
 };
 
 
 onMounted(() => {
-    loadRecipes();
+  loadRecipes();
 });
 </script>
