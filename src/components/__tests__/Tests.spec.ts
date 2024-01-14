c273b296c524dd76733ace605fe2dd4a8f671c83
KoethMoritz <moritzkoeth@gmail.com>
2024-01-14 19:20:56 +0100

    Tests sind grün

---
+++ src/components/__tests__/Tests.spec.ts
@@ -0,0 +1,57 @@
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import HelloWorld from '../HelloWorld.vue';
+import WelcomeItem from '../WelcomeItem.vue';
+import RecipeList from "../RecipeList.vue";
+import TheWelcome from "../TheWelcome.vue";
+import HomeView from "../../views/HomeView.vue";
+import AboutView from "../../views/AboutView.vue";
+
+describe('HelloWorld', () => {
+  it('renders properly', () => {
+    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } });
+    expect(wrapper.text()).toContain('Hello Vitest');
+  });
+});
+
+describe('WelcomeItem', () => {
+  it('renders without errors', () => {
+    const wrapper = mount(WelcomeItem);
+    expect(wrapper).toBeDefined();
+  });
+
+});
+
+describe('RecipeList', () => {
+  it('renders without errors', () => {
+    const wrapper = mount(RecipeList);
+    expect(wrapper).toBeDefined();
+  });
+});
+
+describe('TheWelcome', () => {
+    it('renders without errors', () => {
+      const wrapper = mount(TheWelcome);
+      expect(wrapper).toBeDefined();
+    });
+
+});
+
+describe('HomeView', () => {
+  it('renders without errors', () => {
+    const wrapper = mount(HomeView);
+    expect(wrapper).toBeDefined();
+  });
+
+});
+
+describe('AboutView', () => {
+  it('renders without errors', () => {
+    const wrapper = mount(AboutView);
+    expect(wrapper).toBeDefined();
+  });
+
+});
+
+
+
